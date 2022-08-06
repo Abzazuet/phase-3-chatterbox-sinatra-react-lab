e02fb516276cd289594e25a200086d32e9148ef6
Abel Zazueta <abzazuet@amazon.com>
2022-08-06 05:37:27 -0700

    POST request completed

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,15 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
-  
+  get '/messages' do
+    messages = Message.all.order(created_at: :asc)
+    messages.to_json
+  end
+  post '/messages' do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    message.to_json
+  end
 end
